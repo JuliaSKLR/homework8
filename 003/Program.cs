18748d9a13f60be7066ceefc4d74add133f6fa82
Julia.S <der.lavender@gmail.com>
2022-09-12 14:51:34 +0300

    003

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задайте две матрицы. Напишите программу, которая будет 
 //находить произведение двух матриц.
+//Матрицу C, элементы cij которой вычисляются по следующей формуле: cij=ai0×b0j+ai1×b1j+...+aip×bpj, i=1,...m, j=1,...m
 
 
 int Prompt(string message)
@@ -18,7 +19,7 @@ int[,] CreateMatrix(int rows, int columns)
     {
         for (int j = 0; j < columns; j++)
         {
-            answer[i, j] = rnd.Next(-10, 10);
+            answer[i, j] = rnd.Next(0, 5);
         }
 
     }
@@ -37,4 +38,51 @@ void PrintArray(int[,] array, int rows, int columns)
     }
     Console.WriteLine();
 }
+
+
+int[,] multiplication(int[,] arrayA, int[,] arrayB)
+{
+    int[,] result = new int[arrayA.GetLength(0), arrayB.GetLength(1)];  
+    int sum = 0;
+
+    for (int i = 0; i < arrayA.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < arrayA.GetLength(1); k++)
+            {
+                sum = arrayA[i,k] * arrayB[k,j] + sum;
+            }
+            result[i , j] = sum;
+            sum = 0;
+        }
+
+    }
+    return result;
+
+}
+
+
+
+int rowsa = Prompt("Введите колличество строк в первой матрице  = ");
+int columnsa = Prompt("Введите колличество столбцов в первой матрице =");
+int[,] arrayA = CreateMatrix(rowsa, columnsa);
+int rowsb = Prompt("Введите колличество строк во второй матрице = ");
+int columnsb = Prompt("Введите колличество столбцов во второй матрице =");
+int[,] arrayB = CreateMatrix(rowsb, columnsb);
+PrintArray(arrayA, rowsa, columnsa);
+System.Console.WriteLine();
+PrintArray(arrayB, rowsb, columnsb);
+System.Console.WriteLine();
+    
+if (rowsa != columnsb && columnsa != rowsb)
+{
+    System.Console.WriteLine("Матрицы не могут быть перемножены");
+    return;
+}
+    
+
+int[,] result = multiplication(arrayA, arrayB);
+PrintArray(result, rowsa, columnsb);
 
