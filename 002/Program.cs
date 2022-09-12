43df60710e751ae233853cd78e8f819f0eec8371
Julia.S <der.lavender@gmail.com>
2022-09-12 13:38:53 +0300

    002

--- 002/Program.cs
+++ 002/Program.cs
@@ -24,7 +24,7 @@ int[,] CreateMatrix(int rows, int columns)
     return answer;
 }
 
-void PrintArray(int[,] array, int rows, int columns)
+void PrintMatrix(int[,] array, int rows, int columns)
 {
     for (int i = 0; i < rows; i++)
     {
@@ -37,4 +37,54 @@ void PrintArray(int[,] array, int rows, int columns)
     Console.WriteLine();
 }
 
+int[] SumRows(int[,] array)
+{   
+    int []sumelements = new int [array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        
+                sumelements[i] = array[i, j] + sumelements[i];
+          
 
+        }
+
+    }
+    return sumelements;
+
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+int FindMin(int[] array)            
+{
+    int min = array[0];
+    int indexMin = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+            indexMin = i;
+        }
+
+    }
+    return indexMin;
+    
+}
+int rows = Prompt("Введите колличество строк = ");
+int columns = Prompt("Введите колличество столбцов =");
+int[,] array = CreateMatrix(rows, columns);
+PrintMatrix(array, rows, columns);
+System.Console.WriteLine();
+
+int[] result = SumRows(array);
+PrintArray(result);
+System.Console.WriteLine($"Наименьшая сумма находится в строке {FindMin(result)+1}");
